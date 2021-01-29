9a70161c23b7bf39b2e0e386c51c7e12deaf7f8c
efisio.sarritzu@gmauil.com <efisio.sarritzu@gmail.com>
2021-01-29 12:18:15 +0100

    add splash screen

---
+++ app/src/main/java/com/example/progettotnvandroid/SplashScreen.java
@@ -0,0 +1,36 @@
+package com.example.progettotnvandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+
+        Thread background = new Thread(){
+            public void run(){
+                try {
+                    sleep(5000); //5s = 50000
+
+                    //ora possiamo passare alla main activity
+                    Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                    startActivity(intent);
+
+
+                    finish();
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        background.start();
+    }
+}
