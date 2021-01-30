eaac5ef2f5e7500e09130012ef963cce66456ec1
efisio.sarritzu@gmauil.com <efisio.sarritzu@gmail.com>
2021-01-30 17:15:30 +0100

    creazione nuova list view

---
+++ app/src/main/java/com/example/progettotnvandroid/GestioneUtentiListViewActivity.java
@@ -0,0 +1,14 @@
+package com.example.progettotnvandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GestioneUtentiListViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gestione_utenti_list_view);
+    }
+}
