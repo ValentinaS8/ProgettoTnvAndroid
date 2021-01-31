e6288cd9da340ecc94fa9909566f36611db16683
efisio.sarritzu@gmauil.com <efisio.sarritzu@gmail.com>
2021-01-31 19:05:00 +0100

    test

--- app/src/main/java/com/example/progettotnvandroid/GestisciUtentiActivity.java
+++ app/src/main/java/com/example/progettotnvandroid/GestisciUtentiActivity.java
@@ -3,6 +3,12 @@ package com.example.progettotnvandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import com.example.progettotnvandroid.model.UserList;
+
+import java.util.LinkedList;
+import java.util.List;
 
 public class GestisciUtentiActivity extends AppCompatActivity {
 
@@ -10,5 +16,24 @@ public class GestisciUtentiActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gestisci_utenti);
+
+
+        setContentView(R.layout.utenti_list_view);
+        ListView listView = (ListView)findViewById(R.id.listViewUtenti);
+
+
+        /*List<Contatto> list = new LinkedList<>();
+        list.add(new Contatto("Antonio","Coschignano","1234567890"));
+        list.add(new Contatto("Giovanni","Rossi","1234567890"));
+        list.add(new Contatto("Giuseppe","Bianchi","1234567890"));
+        list.add(new Contatto("Leonardo","Da Vinci","1234567890"));
+        list.add(new Contatto("Mario","Rossi","1234567890"));
+        list.add(new Contatto("Aldo","Rossi","1234567890"));*/
+        /*UtentiListViewAdapter adapter = new UtentiListViewAdapter(this, R.layout.utenti_list_view_row, list);
+        listView.setAdapter(adapter);*/
+
+        UtentiListViewAdapter adapter = new UtentiListViewAdapter(this, R.layout.utenti_list_view_row, UserList.getAllUtenti());
+        listView.setAdapter(adapter);
+
     }
 }
---
+++ app/src/main/java/com/example/progettotnvandroid/UtentiListViewAdapter.java
@@ -0,0 +1,36 @@
+package com.example.progettotnvandroid;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.example.progettotnvandroid.model.Utente;
+
+import java.util.List;
+
+public class UtentiListViewAdapter extends ArrayAdapter<Utente> {
+
+    public UtentiListViewAdapter(Context context, int textViewResourceId,
+                                 List<Utente> objects) {
+        super(context, textViewResourceId, objects);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) getContext()
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        convertView = inflater.inflate(R.layout.utenti_list_view_row, null);
+
+        TextView nome = (TextView)convertView.findViewById(R.id.textViewName);
+        TextView numero = (TextView)convertView.findViewById(R.id.textViewNumber);
+
+        Utente c = getItem(position);
+        nome.setText("abc dfg");
+        numero.setText("123456");
+        return convertView;
+    }
+}
